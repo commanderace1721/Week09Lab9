17de68c0e60ad73a42de4618206e2da1be2e2c04
Law <Law@DESKTOP-SNFMHEC.telus>
2020-10-27 09:22:52 -0600

    part IU
    servlet set up
    
    -l.avestruz

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -5,10 +5,41 @@
  */
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
 /**
  *
  * @author Group CCC
  */
-public class UserServlet {
+public class UserServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+       
+
+
+    }// ends doGet()
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        
+    }// ends doPost()
     
 }
