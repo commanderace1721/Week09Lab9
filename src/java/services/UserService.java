261a4afdf4df1a154ed1c3c2f72184f8fc2053f0
826847 <826847@SAIT224196IT.ACDM.DS.SAIT.CA>
2020-10-28 14:55:48 -0600

    --commit 6-- added return statement and made the sql query into a string

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -6,6 +6,7 @@
 package services;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
@@ -47,61 +48,21 @@ public class UserService {
    
     public int insert(String username, String firstname, String lastname, String password, String email) throws SQLException
     {
-        UserService service = new UserService();
-       String preparedQuery = 
-"INSERT INTO users "
+  
+String preparedQuery =
+        "INSERT INTO users "
 + "(username, password, firstname, lastname,email) "
 + "VALUES "
 + "(?, ?, ?, ?, ?)";
 PreparedStatement ps = 
-NotesDB.prepareStatement(preparedQuery);
-ps.setString(1, service.getUsername());
-ps.setString(2, service.getPassword());
-ps.setString(3, service.getFirstname());
-ps.setString(4, service.getLastname());
-ps.setString(5, service.getEmail());
+connection.prepareStatement(preparedQuery);
+ps.setString(1, username);
+ps.setString(2, password);
+ps.setString(3, firstname);
+ps.setString(4, lastname);
+ps.setString(5, email);
 int rows = ps.executeUpdate(); 
-    }
-    
-    
-      public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+return rows;
     }
     
 }
