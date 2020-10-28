339dd027f7359728d74b7958982038afd293a130
826847 <826847@SAIT224196IT.ACDM.DS.SAIT.CA>
2020-10-28 14:07:03 -0600

    --commit 5-- added some of the insert method

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -37,4 +37,70 @@ public class UserService {
         return rowUpdated;
     }
     
+           
+   private String username;
+   private String firstname;
+   private String lastname;
+   private String password;
+   private String email;
+   
+    public int insert(String username, String firstname, String lastname, String password, String email) throws SQLException
+    {
+        UserService service = new UserService();
+       String preparedQuery = 
+"INSERT INTO users "
++ "(username, password, firstname, lastname,email) "
++ "VALUES "
++ "(?, ?, ?, ?, ?)";
+PreparedStatement ps = 
+NotesDB.prepareStatement(preparedQuery);
+ps.setString(1, service.getUsername());
+ps.setString(2, service.getPassword());
+ps.setString(3, service.getFirstname());
+ps.setString(4, service.getLastname());
+ps.setString(5, service.getEmail());
+int rows = ps.executeUpdate(); 
+    }
+    
+    
+      public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
