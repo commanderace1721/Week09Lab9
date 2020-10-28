f0ea744e8edc5167910b693d7a6678491f08db8b
Haithem Khelif <haithem.khelif@outlook.com>
2020-10-27 19:38:31 -0700

    Custom exception created.

--- src/java/dataaccess/InventoryDBException.java
+++ src/java/dataaccess/InventoryDBException.java
@@ -4,6 +4,15 @@ package dataaccess;
  *
  * @author Group CCC
  */
-public class InventoryDBException {
-    
+public class InventoryDBException extends Exception{
+
+    /**
+     * @author Haithem Khelif 
+     * @version 1.0
+     * @since 10-27-2020
+     * @param message Takes a string and displays it as an error
+     */
+    public InventoryDBException(String message){
+        super(message);
+    }
 }
